66e9dae6d89ccba11e6fdac24f769a89f7bb7964
Johan Henriksson <johanhenriksson@live.com>
2026-02-02 18:33:40 +0100

    feat: version command

---
+++ cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var Version = "dev"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number",
+	Args:  cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
